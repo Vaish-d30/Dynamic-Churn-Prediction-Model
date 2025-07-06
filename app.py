f684e87720eb48c51aa2e8d70bc867073409dfd7
Vaishnavi Desai <vaishnavidesai3028@gmail.com>
2025-07-06 21:34:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,8 @@ def predict():
     except Exception as e:
         return f"An error occurred: {str(e)}", 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
